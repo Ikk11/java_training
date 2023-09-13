887348bfdf16cbf300293aa693d5d3a75a0977f9
Ikk11 <wom8693@gmail.com>
2023-09-13 21:46:20 +0900

    Scanner

---
+++ src/_06_Scanner.java
@@ -0,0 +1,7 @@
+import java.util.Scanner; // util.Scanner를 import 시켜야한다.
+public class _06_Scanner {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc);
+    }
+}
