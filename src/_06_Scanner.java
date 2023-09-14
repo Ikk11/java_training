6f502a0fa660c22d209377b2b884b0de4ffdb40c
Ikk11 <wom8693@gmail.com>
2023-09-14 20:29:41 +0900

    Scanner

--- src/_06_Scanner.java
+++ src/_06_Scanner.java
@@ -2,6 +2,10 @@ import java.util.Scanner; // util.Scanner를 import 시켜야한다.
 public class _06_Scanner {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println(sc);
+        String z = sc.nextLine(); // String 한줄 불러오기 but 개행도 불러오기 때문에 첫째로 써야함
+        int x = sc.nextInt(); // int값 불러오기
+        String y = sc.next(); // String 한단어 불러오기
+        long d = sc.nextLong(); // long 값 불러오기
+
     }
 }
