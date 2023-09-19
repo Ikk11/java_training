6f75dcbbe39063222e333d16082fa3bbd1ebc4b5
Ikk11 <wom8693@gmail.com>
2023-09-19 20:20:15 +0900

    Printf

--- src/_09_Printf.java
+++ src/_09_Printf.java
@@ -3,7 +3,7 @@ public class _09_Printf {
         int x = 10;
         double y = 10.0;
         String z = "십";
-                            // int는 %d, double은 %f, String은 %s, 마지막엔 \n으로 개행해준다.
+                            // int는 %d, double은 %f, String은 %s, 마지막엔 \n으로 개행해준다
         System.out.printf("인트값 : %d, 더블값 : %f, 스트링값 : %s\n", x, y, z);
     }
 }
